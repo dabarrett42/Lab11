197b0e2f7e4685f4e2cee9c266fff2e3b05ddba2
Ernest Cody Sorrell <cody.sorrell@gmail.com>
2014-11-06 14:23:13 -0600

    Fixed Driver
    Testing for to array, and tree sort

--- BSTDriver.cpp
+++ BSTDriver.cpp
@@ -46,6 +46,11 @@ int main()
    //DO THIS
    //test your tree sort method
    CD** unsorted_cds = cds->toArray();
+   for(int v = 0; v < num_items; v++)
+   {
+		CD* cd = unsorted_cds[v];
+		cd->displayCD();
+   }
    CD** sorted_cds = bst->treeSort(unsorted_cds, num_items, &CD::compare_items, &CD::compare_keys);
    for(int i = 0; i < num_items; i++)
    {
