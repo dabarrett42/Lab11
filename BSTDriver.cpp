b3fa125b35d3c56997c8ec2a91b7de5f8ac08cc5
Ernest Cody Sorrell <cody.sorrell@gmail.com>
2014-11-06 13:57:17 -0600

    Sort

--- BSTDriver.cpp
+++ BSTDriver.cpp
@@ -46,7 +46,7 @@ int main()
    //DO THIS
    //test your tree sort method
    CD** unsorted_cds = cds->toArray();
- //  CD** sorted_cds = bst->treeSort(unsorted_cds, );
+   CD** sorted_cds = bst->treeSort(unsorted_cds, num_items, &CD::compare_items, &CD::compare_keys);
 
 
 
